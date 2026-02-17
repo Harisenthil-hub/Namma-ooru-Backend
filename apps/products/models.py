76d561b6c1723b239d4c20b82f4364a87665724c
Harisenthil-hub <hari367854@gmail.com>
2026-02-17 17:27:58 +0530

    Added Variant Table Created Filter, Search and Search Suggestions api

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=100,unique=True,db_index=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -12,7 +12,7 @@ class Category(models.Model):
     
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,db_index=True)
     price = models.DecimalField(max_digits=10,decimal_places=2)
     image = models.ImageField(upload_to='products/')
     category = models.ForeignKey(
@@ -38,4 +38,22 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='variants'
+    )
+    
+    weight = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    offer_price = models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
+    is_active = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.product.name} - {self.weight}"
     
--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from .views import ProductListAPIView, HomeProductAPIView, CategoryListAPIView
+from .views import ProductListAPIView, HomeProductAPIView, CategoryListAPIView, ProductSearchSuggestionAPIView
 from .admin_views import AdminProductListAPIView, AdminProductCreateAPIView, AdminProductUpdateAPIView, AdminProductDeleteAPIView
+from .admin_views import AdminProductVariantCreateAPIView, AdminProductVariantUpdateAPIView, AdminProductVariantDeleteAPIView
 
 urlpatterns = [
     path('home/',HomeProductAPIView.as_view(),name='home-products'),
     path('list-products/',ProductListAPIView.as_view(),name='product-list'),
     path('categories/',CategoryListAPIView.as_view(),name='category-list'),
+    path('search-suggestions/',ProductSearchSuggestionAPIView.as_view(),name='product-search-suggestions'),
     path('admin-products/',AdminProductListAPIView.as_view(),name='admin-products-list'),
     path('create-product/',AdminProductCreateAPIView.as_view(),name='create-product'),
     path('update-product/<int:id>/',AdminProductUpdateAPIView.as_view(),name='update-product'),
     path('delete-product/<int:id>/',AdminProductDeleteAPIView.as_view(),name='delete-product'),
+    path('variants/',AdminProductVariantCreateAPIView.as_view(),name='create-variant'),
+    path('variants/<int:id>/',AdminProductVariantUpdateAPIView.as_view(),name='update-variant'),
+    path('variants/<int:id>/',AdminProductVariantDeleteAPIView.as_view(),name='delete-variant'),
 ]
